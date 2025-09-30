15048271ac270712f4192938b9c4b84c86214c76
martssmt <lozanomarcosmarta@gmail.com>
2025-09-30 20:39:24 +0200

    Project Structure

--- src/main/java/es/upm/etsisi/poo/App.java
+++ src/main/java/es/upm/etsisi/poo/App.java
@@ -1,9 +1,5 @@
 package es.upm.etsisi.poo;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
---
+++ src/main/java/es/upm/etsisi/poo/Model/Product.java
@@ -0,0 +1,4 @@
+package es.upm.etsisi.poo.Model;
+
+public class Product {
+}
