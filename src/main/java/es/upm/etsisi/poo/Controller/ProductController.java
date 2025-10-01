b3d73fcdcca4ac61e18bc7c8cf6ff0427bbea31a
martssmt <lozanomarcosmarta@gmail.com>
2025-10-01 18:49:49 +0200

    feat(controller): ProductController-> Constructor and getter implemented

--- src/main/java/es/upm/etsisi/poo/Controller/ProductController.java
+++ src/main/java/es/upm/etsisi/poo/Controller/ProductController.java
@@ -1,4 +1,21 @@
 package es.upm.etsisi.poo.Controller;
 
+import es.upm.etsisi.poo.Model.Catalog;
+import es.upm.etsisi.poo.View.ConsoleView;
+
 public class ProductController {
+
+    private Catalog catalog;
+    private ConsoleView view;
+
+    public ProductController(ConsoleView view) {
+        this.catalog = new Catalog();
+        this.view = view;
+    }
+
+    public Catalog getCatalog() {
+        return catalog;
+    }
+
+
 }
