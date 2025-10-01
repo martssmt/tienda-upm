cb662abb15f986cacb43eb48fbf901f44bf3e787
martssmt <lozanomarcosmarta@gmail.com>
2025-10-01 19:02:15 +0200

    feat(controller): ProductController-> handleAdd() and handlePrint() implemented

--- src/main/java/es/upm/etsisi/poo/Controller/ProductController.java
+++ src/main/java/es/upm/etsisi/poo/Controller/ProductController.java
@@ -1,6 +1,8 @@
 package es.upm.etsisi.poo.Controller;
 
 import es.upm.etsisi.poo.Model.Catalog;
+import es.upm.etsisi.poo.Model.Category;
+import es.upm.etsisi.poo.Model.Product;
 import es.upm.etsisi.poo.View.ConsoleView;
 
 public class ProductController {
@@ -15,6 +17,22 @@ public class ProductController {
 
     public Catalog getCatalog() {
         return catalog;
+    }
+
+    public void handleAdd(int id, String name, Category category, double price) {
+        Product product = new Product(id, name, category, price);
+        boolean productAdded=catalog.addProduct(product);
+        if (!productAdded) {
+            this.view.showMessage("Product with id " + id + " already exists in the catalog.");
+            this.view.showMessage("prod add: error");
+        } else {
+            this.handlePrint(product);
+            this.view.showMessage("prod add: ok");
+        }
+    }
+
+    public void handlePrint(Product product) {
+        this.view.showProduct(product.toString());
     }
 
 
