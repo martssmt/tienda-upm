37e730e51aebed6605e0a7840f414e8eee212164
tomastte <juarezgelardoto@gmail.com>
2025-10-01 12:06:18 +0200

    feat(model): implement Category class

--- src/main/java/es/upm/etsisi/poo/Model/Category.java
+++ src/main/java/es/upm/etsisi/poo/Model/Category.java
@@ -1,4 +1,19 @@
 package es.upm.etsisi.poo.Model;
 
-public class Category {
+public enum Category {
+    MERCH(0),
+    PAPELERIA(0.05),
+    ROPA(0.07),
+    LIBRO(0.1),
+    ELECTRONICA(0.03);
+
+    private final double discount;
+
+    Category(double discount) {
+        this.discount=discount;
+    }
+
+    public double getValueDiscount(){
+        return this.discount;
+    }
 }
