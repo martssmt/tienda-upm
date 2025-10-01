0e519eea600b21c1fe46754ce0b8742006b61108
tomastte <juarezgelardoto@gmail.com>
2025-10-01 18:41:40 +0200

    feat(model): method addProduct implemented in Catalog class

--- src/main/java/es/upm/etsisi/poo/Model/Catalog.java
+++ src/main/java/es/upm/etsisi/poo/Model/Catalog.java
@@ -1,4 +1,27 @@
 package es.upm.etsisi.poo.Model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Catalog {
+    private static final int MAX_PRODUCTS = 200;
+    private final Map<Integer, Product> productsList;
+
+    public Catalog() {
+        this.productsList = new HashMap<>();
+    }
+
+    public boolean addProduct(Product product) {
+
+        if (this.productsList.size()>=MAX_PRODUCTS){
+            return false;
+        }
+
+        if (this.productsList.containsKey(product.getId())){
+            return false;
+        }
+
+        this.productsList.put(product.getId(), product);
+        return true;
+    }
 }
