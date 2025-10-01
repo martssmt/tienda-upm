2d4361297aa1407c9735c817690daae6549de30f
martssmt <lozanomarcosmarta@gmail.com>
2025-10-01 17:55:37 +0200

    feat(controller): TicketController-> Constructor and handleNew() implemented

--- src/main/java/es/upm/etsisi/poo/Controller/TicketController.java
+++ src/main/java/es/upm/etsisi/poo/Controller/TicketController.java
@@ -1,4 +1,24 @@
 package es.upm.etsisi.poo.Controller;
 
+import es.upm.etsisi.poo.Model.Catalog;
+import es.upm.etsisi.poo.Model.Ticket;
+import es.upm.etsisi.poo.View.ConsoleView;
+
 public class TicketController {
+
+    private Ticket ticket;
+    private ConsoleView view;
+    private Catalog catalog;
+
+    public TicketController(ConsoleView view, Catalog catalog) {
+        this.view = view;
+        this.catalog = catalog;
+        this.ticket = new Ticket();
+    }
+
+    public void handleNew() {
+        ticket.clear();
+        this.view.showMessage("ticket new: ok");
+    }
+
 }
