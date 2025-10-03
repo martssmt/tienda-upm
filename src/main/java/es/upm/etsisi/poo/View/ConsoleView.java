d4f2b6380b9326217a269d7d1946f68d9c1ed5cf
sofiam32 <sofiamerfer@gmail.com>
2025-10-03 14:07:21 +0200

    feat(view): method showHelp created in ConsoleView class

--- src/main/java/es/upm/etsisi/poo/View/ConsoleView.java
+++ src/main/java/es/upm/etsisi/poo/View/ConsoleView.java
@@ -1,7 +1,28 @@
 package es.upm.etsisi.poo.View;
 
 public class ConsoleView {
+
     public void showMessage(String message) {
         System.out.println(message);
+    }
+
+    public static void showHelp() {
+        final String MESSAGE = """
+        Commands:
+         prod add <id> "<name>" <category> <price>
+         prod list
+         prod update <id> NAME|CATEGORY|PRICE <value>
+         prod remove <id>
+         ticket new
+         ticket add <prodId> <quantity>
+         ticket remove <prodId>
+         ticket print
+         echo "<texto>"
+         help
+         exit
+        
+        Categories: MERCH, STATIONERY, CLOTHES, BOOKS, ELECTRONICS
+        Discounts if there are ≥2 units in the category: MERCH 0%, STATIONERY 5%, CLOTHES 7%, BOOKS 10%, ELECTRONICS 3%.""";
+        System.out.println(MESSAGE);
     }
 }
