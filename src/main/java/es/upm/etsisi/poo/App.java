cbe00662ce753e9e8b65341ae1578c62a91d424c
sofiam32 <sofiamerfer@gmail.com>
2025-10-07 10:18:10 +0200

    fix(view): Two intro messages had been added and the two final messages have been deleted and ConsoleView object have been deleted and ticketController and command stops asking for view

--- src/main/java/es/upm/etsisi/poo/App.java
+++ src/main/java/es/upm/etsisi/poo/App.java
@@ -1,33 +1,27 @@
 package es.upm.etsisi.poo;
 
-
-import es.upm.etsisi.poo.Controller.CommandController;
-import es.upm.etsisi.poo.Controller.ProductController;
-import es.upm.etsisi.poo.Controller.TicketController;
+import es.upm.etsisi.poo.Controller.*;
 import es.upm.etsisi.poo.Model.Catalog;
-import es.upm.etsisi.poo.View.ConsoleView;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
 
 public class App {
     public static void main(String[] args) {
         try {
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            ConsoleView view = new ConsoleView();
-            ProductController productController = new ProductController(view);
+            ProductController productController = new ProductController();
             Catalog catalog = productController.getCatalog();
-            TicketController ticketController = new TicketController(view, catalog);
-            CommandController comand = new CommandController(productController, ticketController, view);
-            String line = "";
-            while (!line.equals("exit")) {
+            TicketController ticketController = new TicketController(catalog);
+            CommandController command = new CommandController(productController, ticketController);
+            System.out.println("Welcome to the ticket module App.");
+            System.out.println("Ticket module. Type 'help' to see commands.");
+            while (true) {
                 System.out.print("tUPM> ");
-                line = reader.readLine();
-                comand.parseCommand(line);
+                String line = reader.readLine();
+                command.parseCommand(line);
             }
-            System.out.println("Closing application.");
-            System.out.println("Goodbye!");
         } catch (IOException exception) {
             throw new RuntimeException(exception);
         }
