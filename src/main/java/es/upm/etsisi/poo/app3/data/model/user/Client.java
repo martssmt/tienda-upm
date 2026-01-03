6959c6c4aacccd3a85932359076ddfe7e004ca4f
jl721 <jilingli1227@gmail.com>
2026-01-03 21:34:52 +0100

    feat(model): ClientType enum implemented and add clientType attribute to Client class

--- src/main/java/es/upm/etsisi/poo/app3/data/model/user/Client.java
+++ src/main/java/es/upm/etsisi/poo/app3/data/model/user/Client.java
@@ -4,13 +4,15 @@ import es.upm.etsisi.poo.app3.data.model.exceptions.InvalidAttributeException;
 
 public class Client extends User {
     private String cashierId;
+    private final ClientType clientType;
 
-    public Client(String name, String mail, String cashierId) {
+    public Client(String name, String mail, String cashierId,  ClientType clientType) {
         super(name, mail);
         if (!cashierId.matches("UW[0-9]{7}")) {
             throw new InvalidAttributeException("Invalid cashierId");
         }
         this.cashierId = cashierId;
+        this.clientType = clientType;
     }
 
     public String getCashierId() {
@@ -24,10 +26,28 @@ public class Client extends User {
         this.cashierId = cashierId;
     }
 
+    public ClientType getClientType() {
+        return this.clientType;
+    }
+
     @Override
     public void setId(String id) {
-        if (id.length() != 9)
-            throw new InvalidAttributeException("Invalid DNI");
+        if(clientType == null){
+            throw new InvalidAttributeException("ClientType is null");
+        }
+
+        switch(clientType) {
+            case COMPANY:
+                if (!id.matches("[A-Za-z][0-9]{8}")) {
+                    throw new InvalidAttributeException("Invalid NIF for company");
+                }
+                break;
+            case PERSON:
+                if (id.length() != 9) {
+                    throw new InvalidAttributeException("Invalid DNI");
+                }
+                break;
+        }
         this.id = id;
     }
 
--- src/main/java/es/upm/etsisi/poo/app3/data/model/user/ClientType.java
+++ src/main/java/es/upm/etsisi/poo/app3/data/model/user/ClientType.java
@@ -1,4 +1,6 @@
 package es.upm.etsisi.poo.app3.data.model.user;
 
 public enum ClientType {
+    PERSON,
+    COMPANY
 }
