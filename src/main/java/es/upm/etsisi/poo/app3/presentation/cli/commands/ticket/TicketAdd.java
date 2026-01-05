c2d2649ac5ee084d26fec27e546a455ccac07156
sofiam32 <sofiamerfer@gmail.com>
2026-01-05 15:24:52 +0100

    TicketAdd updated

--- src/main/java/es/upm/etsisi/poo/app3/presentation/cli/commands/ticket/TicketAdd.java
+++ src/main/java/es/upm/etsisi/poo/app3/presentation/cli/commands/ticket/TicketAdd.java
@@ -1,5 +1,6 @@
 package es.upm.etsisi.poo.app3.presentation.cli.commands.ticket;
 
+import es.upm.etsisi.poo.app3.data.model.shop.products.BasicProduct;
 import es.upm.etsisi.poo.app3.data.model.shop.products.CustomProduct;
 import es.upm.etsisi.poo.app3.data.model.shop.products.Product;
 import es.upm.etsisi.poo.app3.data.model.shop.ticket.Ticket;
@@ -57,8 +58,10 @@ public class TicketAdd implements Command {
         if (product instanceof CustomProduct) {
             String[] texts = java.util.Arrays.copyOfRange(params, 4, params.length);
             ticket = this.cashierService.addCustomProduct(cashid, ticketid, (CustomProduct) product, amount, texts);
-        } else {
+        } else if (product instanceof BasicProduct){
             ticket = this.cashierService.addProduct(cashid, ticketid, product, amount);
+        } else {
+            ticket = this.cashierService.addService(cashid, ticketid, product);
         }
         this.view.showEntity(ticket);
         this.view.show("ticket add: ok");
